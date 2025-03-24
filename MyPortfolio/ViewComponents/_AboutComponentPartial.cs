b8f7c4fa2113741e190432e7d213589ccc6724f2
Enis Can Yıldız <eniscanyldz_1282_@hotmail.com>
2025-03-24 19:13:25 +0300

    Add project files.

---
+++ MyPortfolio/ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _AboutComponentPartial: ViewComponent
+    {
+        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.aboutTitle = portfolioContext.Abouts.Select(i=>i.Title).FirstOrDefault();
+            ViewBag.aboutSubDescription = portfolioContext.Abouts.Select(i => i.SubDescription).FirstOrDefault();
+            ViewBag.aboutDetail = portfolioContext.Abouts.Select(i => i.Details).FirstOrDefault();
+            return View();
+        }
+    }
+}
